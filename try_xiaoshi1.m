e4b3cc29a88ced68c4cd9c8e742eaab6882f47af
lxiaoshi95 <32397337+lxiaoshi95@users.noreply.github.com>
2018-05-31 23:41:58 -0700

    Add files via upload

---
+++ try_xiaoshi1.m
@@ -0,0 +1,68 @@
+%% clear
+close all; clc; clear;
+
+%%
+fs = 40; %sampling frequency
+ret = read_log_file('out_0508_1159');
+[n_packets,~] = size(ret);
+data1 = zeros(1,n_packets);
+data2 = zeros(1,n_packets);
+nr1 = 1;
+nc1 = 1;
+nr2 = 1;
+nc2 = 2;
+pac = 1:n_packets;
+channel_info = zeros(6,n_packets);
+cur_channel = 1;
+for subcarrier = 56:4:56
+    for packet = 1:n_packets
+        csi_data = ret{packet}.csi;
+        if(csi_data ~= 0)
+            data1(1,packet) = csi_data(nr1,nc1,subcarrier);
+            data2(1,packet) = csi_data(nr2,nc2,subcarrier);
+        end
+    end
+    ang1 = angle(data1);
+    ang2 = angle(data2);
+    x = pac;
+    y = mod(ang1 - ang2, 2*pi);
+    opol = 5;
+    [p,s,mu] = polyfit(x, y, opol);
+    f_y = polyval(p, x, [], mu);
+    figure
+    plot(x, y, 'r', x, f_y, 'b')
+    legend('Signal', 'Baseline')
+    set(gca, 'FontSize', 15)
+    y = y - f_y;
+    figure
+    plot(x, y, 'r')
+    set(gca, 'FontSize', 15)
+    figure
+    wavename='cmor3-3';
+    totalscal = 2048; %尺度序列的长度，即scal的长度
+    wcf = centfrq(wavename); %小波的中心频率
+    cparam = 2 * wcf * totalscal; %为得到合适的尺度所求出的参数
+    a = totalscal:-1:1;
+    scal = cparam ./ a; %得到各个尺度，以使转换得到频率序列为等差序列
+    coefs = cwt(y, scal, wavename); %得到小波系数
+    f = scal2frq(scal, wavename, 1/fs); %将尺度转换为频率
+    imagesc(x/40, f, abs(coefs)); %绘制色谱图
+    xlabel('time t/s');
+    ylabel('frequency f/Hz');
+    title('time-frequency presentation');
+    ylim([0 3])
+    set(gca, 'FontSize', 20)
+    set(gca, 'position', [0.12, 0.12, 0.8, 0.8])
+    set(gcf, 'position', [100 100 900 800])
+    figure
+    yy = abs(coefs);
+    yy(find(f<1), :) = 0;
+    imagesc(x/40, f, yy); %绘制色谱图
+    xlabel('time t/s');
+    ylabel('frequency f/Hz');
+    title('time-frequency presentation');
+    ylim([1 2])
+    set(gca, 'FontSize', 20)
+    set(gca, 'position', [0.12, 0.12, 0.8, 0.8])
+    set(gcf, 'position', [100 100 900 800])
+end
